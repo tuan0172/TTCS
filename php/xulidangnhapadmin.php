fd19dcd536910634ee0f78b9281c03acfced77e8
Tuan Anh <tuananhnguyenduc2710@gmail.com>
2022-12-26 21:20:08 +0700

    admin

--- php/xulidangnhapadmin.php
+++ php/xulidangnhapadmin.php
@@ -1,24 +1,24 @@
 <?php
-// session_start();
-// $taikhoan = $_POST['data_username'];
-// $matkhau = $_POST['data_password'];
+session_start();
+$taikhoan = $_POST['data_username'];
+$matkhau = $_POST['data_password'];
 
-// // Sau khi dang nhap
-// require("../BackEnd/ConnectionDB/DB_driver.php");
+// Sau khi dang nhap
+require("../BackEnd/ConnectionDB/DB_driver.php");
 
-// $db = new DB_driver();
-// $db->connect();
+$db = new DB_driver();
+$db->connect();
 
-// $taikhoan = mysqli_escape_string($db->__conn, $taikhoan);
-// $matkhau = mysqli_escape_string($db->__conn, $matkhau);
+$taikhoan = mysqli_escape_string($db->__conn, $taikhoan);
+$matkhau = mysqli_escape_string($db->__conn, $matkhau);
 
-// // mysqli_set_charset($connSanPham,"utf8");
-// $sql = "SELECT * FROM nguoidung WHERE TaiKhoan = '$taikhoan' AND MatKhau='$matkhau' AND MaQuyen!='1' AND TrangThai='1'";
+// mysqli_set_charset($connSanPham,"utf8");
+$sql = "SELECT * FROM nguoidung WHERE TaiKhoan = '$taikhoan' AND MatKhau='$matkhau' AND MaQuyen!='1' AND TrangThai='2'";
 
-// $dsad = $db->get_list($sql);
+$dsad = $db->get_list($sql);
 
-// if (sizeof($dsad) > 0) {
-//     $_SESSION['currentUser'] = $dsad[0];
-// } else  echo "no";
+if (sizeof($dsad) > 0) {
+    $_SESSION['currentUser'] = $dsad[0];
+} else  echo "no";
 
-// $db->dis_connect();
+$db->dis_connect();
